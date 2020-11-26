271b2554b1ba89f7a257a793d1ec58c3403ed9fe
arkaprabhadey <euphoricdey420@gmail.com>
2020-11-26 11:10:57 +0530

    [Arka]Refactored:App.js file to make it responsive using set and onChange

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,34 +1,46 @@
-import logo from './logo.svg';
+import logo from './assets/BL_logo_square_jpg.jpg';
 import './App.css';
 import React from 'react';
-
-// function App() {
-//   return (
-//    <div className="App">
- 
-//         <h1>
-//           Hello from BridgeLabz
-//         </h1>
-   
-//      </div>
-//   );
-// }
 class App extends React.Component{
+  url="https://www.bridgelabz.com";
   constructor(){
     super();
     this.state={
-      title:"Hello From BridgeLabz"
+      title:"Hello From BridgeLabz",
+      usernName:'',
+      nameError: ''
+    }
+  }
+
+  onClick= ($event)=>{
+    console.log("Save button is clicked",$event);
+    window.open(this.url,"_blank");
+  }
+  onNameChange=(event) =>{
+    console.log("Value is: ",event.target.value);
+    const nameRegex=RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    this.setState({usernName: event.target.value})
+    if(nameRegex.test(event.target.value)){
+      this.setState({nameError: ''})
+    }
+    else{
+      this.setState({nameError:'Name is incorrect'})
     }
   }
   render(){
     return (
-      <div className="App">
-    
-           <h1>
-             {this.state.title}
+      <div>
+        <div >
+          <h1>
+             Hello {this.state.usernName} from BridgeLabz
            </h1>
-      
+           <img src={logo} onClick={this.onClick} alt="Employability delivered"/>
         </div>
+        <div>
+        <input onChange={this.onNameChange}/>
+        <span className="error">{this.state.nameError}</span>
+       </div>
+      </div>
      );
   }
 }
